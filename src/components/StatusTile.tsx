cec8e35340d27182eed1831ef3e2f1bde6187afc
amalkisanae <amalkisana@gmail.com>
2025-04-08 17:18:03 -0600

    sizefont

--- src/components/StatusTile.tsx
+++ src/components/StatusTile.tsx
@@ -9,8 +9,8 @@ export default function StatusTile({
 }: StatusTileProps) {
   return (
     <div className="border-[5px] border-[#3b522f] rounded-md px-8 pt-2 pb-4 bg-[#ffffffcc] text-center flex-shrink min-w-[5px]">
-      <h2 className="mt-0 mb-2 text-[#3b522f] text-[2rem]">{`${measurementName} Status`}</h2>
-      <p className="text-[#3b522f] text-[3rem] font-bold m-0">{value}</p>
+      <h2 className="mt-0 mb-2 text-[#3b522f] text-[0.5rem]  md:text-[2rem]">{`${measurementName} Status`}</h2>
+      <p className="text-[#3b522f] text-[1rem]  md:text-[3rem] font-bold m-0">{value}</p>
     </div>
   );
 }
