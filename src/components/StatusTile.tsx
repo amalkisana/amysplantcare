651005b7c3c37c1585045e39560183fc123225e5
amalkisanae <amalkisana@gmail.com>
2025-04-08 17:24:42 -0600

    shrinkfit

--- src/components/StatusTile.tsx
+++ src/components/StatusTile.tsx
@@ -8,7 +8,7 @@ export default function StatusTile({
   value,
 }: StatusTileProps) {
   return (
-    <div className="border-[5px] border-[#3b522f] rounded-md px-8 pt-2 pb-4 bg-[#ffffffcc] text-center flex-shrink min-w-[100px]">
+    <div className="border-[5px] border-[#3b522f] rounded-md px-8 pt-2 pb-4 bg-[#ffffffcc] text-center flex-shrink">
       <h2 className="mt-0 mb-2 text-[#3b522f] text-[1rem]  md:text-[2rem]">{`${measurementName} Status`}</h2>
       <p className="text-[#3b522f] text-[1.5rem]  md:text-[3rem] font-bold m-0">{value}</p>
     </div>
