f2902d586bf4757a995bd395fad1288da545ef6b
amalkisanae <amalkisana@gmail.com>
2025-04-09 15:10:35 -0600

    freqname

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -43,9 +43,9 @@ export function EditForm({
               onChange={(e) => setWaterFrequency(e.target.value)}
             >
               <option value="0">Daily</option>
-              <option value="1">Semi-Weekly</option>
+              <option value="1">Semiweekly</option>
               <option value="2">Weekly</option>
-              <option value="3">Bi-Weekly</option>
+              <option value="3">Biweekly</option>
               <option value="4">Monthly</option>
             </select>
           </div>
